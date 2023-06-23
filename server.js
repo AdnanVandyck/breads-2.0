69fc051659ddb51c4f871a7e5e6b1118264a56a8
Adnan <adnanvandyck@gmail.com>
2023-06-23 19:50:45 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT
 
+app.use(express.static('public'))
 
 //MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
@@ -22,7 +24,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
-
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
 
 
 app.listen(PORT, () => {
