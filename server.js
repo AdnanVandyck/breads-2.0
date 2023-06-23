5aac025a4e6636077573cb30aa22f23cf679a030
Adnan <adnanvandyck@gmail.com>
2023-06-23 19:21:30 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const express = require('express')
 
 // CONFIGURATION
 require('dotenv').config()
+const app = express();
 const PORT = process.env.PORT
-const app = express()
+
+
+//MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
 // ROUTES
+
+//HOMEPAGE
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+
+
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
